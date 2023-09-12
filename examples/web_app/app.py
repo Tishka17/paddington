c2b39ebb658d479650bea41b4a53708268024157
Andrey Tikhonov <andrey.tikhonov@quantori.com>
2023-09-13 01:33:58 +0400

    http status codes

--- examples/web_app/app.py
+++ examples/web_app/app.py
@@ -2,32 +2,34 @@ import json
 
 from wsgi_app import App, WsgiContext
 from wsgi_switch import WsgiSwitch
+from http import HTTPStatus
 
 users = []
 router = WsgiSwitch()
 
 
+def status(http_status: HTTPStatus):
+    return f"{http_status.value} {http_status.phrase}"
+
+
 @router.track("/", methods=["GET"])
 def index(environ, context: WsgiContext):
-    status = '200 OK'
     response_headers = [('Content-type', 'text/plain')]
-    context.start_response(status, response_headers)
+    context.start_response(status(HTTPStatus.OK), response_headers)
     return [b"Hello world!\n"]
 
 
 @router.track("/users", methods=["GET"])
 def get_users(environ, context: WsgiContext):
-    status = '200 OK'
     response_headers = [('Content-type', 'application/json')]
-    context.start_response(status, response_headers)
+    context.start_response(status(HTTPStatus.OK), response_headers)
     return [json.dumps(users).encode()]
 
 
 @router.track("/users", methods=["POST"])
 def add_user(environ, context: WsgiContext):
-    status = '200 OK'
     response_headers = [('Content-type', 'application/json')]
-    context.start_response(status, response_headers)
+    context.start_response(status(HTTPStatus.OK), response_headers)
     users.append({"user": len(users)})
     return [b"{}"]
 
